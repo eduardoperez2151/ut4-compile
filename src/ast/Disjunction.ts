2367f391dbcf4fd21e9758bc59e70926e486ade9
valentin11 <valentinfer11@gmail.com>
2017-10-12 18:46:13 -0300

    Se implementa and or neg

--- src/ast/Disjunction.ts
+++ src/ast/Disjunction.ts
@@ -23,7 +23,10 @@ export class Disjunction implements Exp {
   }
 
   compileCIL(context: CompilationContext): CompilationContext {
-    return undefined;
+    this.lhs.compileCIL(context);
+    this.rhs.compileCIL(context);
+    context.appendInstruction("or");
+    return context;
   }
 
   maxStackIL(value: number): number {
